9308646da21b31d1e3f1ef47d945e3efeee4dbb0
bijonl <bijon2591@gmail.com>
2024-01-25 15:12:28 -0500

    more routing updates

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
 import TicketCard from './(components)/TicketCard'
 
-const Dashboard = () => {
+const getTickets = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/Tickets", {
+      cache: "no-store"
+    })
+
+    return res.json()
+  } catch (error) {
+    console.log("Failed to get ticket", error)
+  }
+}
+
+
+const Dashboard = async () => {
+  
   return (
     <div className="p-5">
       <div className="lg:grid grid-cols-2 xl:grid-cols-4">
